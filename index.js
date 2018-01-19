5801a8322e6c637954c00f769ee90cc2a058d6fc
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-19 23:36:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- var randomPrice = Math.floor(Math.random() * 100)
+ var itemPrice = Math.floor(Math.random() * 100)
  var newItems = { [item]: itemPrice };
  cart.push(item);
 }
